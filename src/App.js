d9e3f1b5dca3400a59a0cfbb165f855ac6326b46
johnnam1121 <johnnam93@gmail.com>
2023-02-11 23:23:37 -0600

    testing hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import UnitsList from './components/pages/UnitsList';
@@ -11,14 +11,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter basename="starcraft2-unit-counter">
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/UnitsList" element={<UnitsList />} />
             <Route path="/UnitCountersPage" element={<UnitCountersPage />} />
             <Route path="/ArmyCounterPage" element={<ArmyCounterPage />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     )
   }
